c5f4bf20d2dd3b1de4c065529cb4093566d5cca7
Eduardo Vítor <eduardovitor730@gmail.com>
2023-09-19 20:21:53 -0300

    bugfix: convenção do telegram de nomes de comandos

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ API_KEY = os.environ["API_TELEGRAM_BOT"]
 bot = telebot.TeleBot(API_KEY)
 msg='Olá, eu sou o ArapiracaNewsBot'
 msg2='Criado por Eduardo Vítor, meu objetivo é fornecer as principais notícias dos jornais de Arapiraca de forma simples e sem esforço',
-msg3='Você têm 4 comandos nesse bot:\n/7segundos: mostra as notícias do slideshow do site 7segundos\n/diarioArapiraca: mostra as notícias mais lidas do site Diário Arapiraca\n/jaenoticia: mostra as notícias do slideshow do site Já é notícia\n/help: mostra novamente essa ajuda\n\nÉ isso, se mantenha informado! :)'
+msg3='Você têm 4 comandos nesse bot:\n/7segundos: mostra as notícias do slideshow do site 7segundos\n/diarioarapiraca: mostra as notícias mais lidas do site Diário Arapiraca\n/jaenoticia: mostra as notícias do slideshow do site Já é notícia\n/help: mostra novamente essa ajuda\n\nÉ isso, se mantenha informado! :)'
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -18,7 +18,7 @@ def welcome(message):
 def help(message):
   bot.send_message(message.chat.id, msg3)
 
-@bot.message_handler(commands=['diarioArapiraca'])
+@bot.message_handler(commands=['diarioarapiraca'])
 def abrir_noticias_diario(message):
   response=arapiracanews.comando_news("Diário Arapiraca News\n\n")
   bot.send_message(message.chat.id, response)
