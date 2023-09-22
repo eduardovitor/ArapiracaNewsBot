c631bb8eb2ea4c75db72897126014a9d11b68b02
Eduardo Vítor <eduardovitor730@gmail.com>
2023-09-22 20:25:55 -0300

    feat: testes unitários da lógica de negócio

---
+++ test/formatacao_test.py
@@ -0,0 +1,18 @@
+import unittest
+import sys
+sys.path.append('..')
+from arapiracanews import formatar_msg
+import json
+
+class FormatacaoTest(unittest.TestCase):
+    def test_formatar(self):
+        site_mock = "Já é notícia News\n\n"
+        with open('noticias.json') as json_file:
+            noticias_mock = json.load(json_file)
+        msg = formatar_msg(noticias_mock,site_mock)
+        self.assertIsNotNone(msg, "Retorno da formatação não deve ser nulo")
+        self.assertIsInstance(msg,str, "Mensagem deve ser uma string")
+        self.assertTrue(len(msg) > 0, "A mensagem deve ter vários caracteres") 
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/validacao_test.py
@@ -0,0 +1,18 @@
+import unittest
+import sys
+sys.path.append('..')
+import json
+from arapiracanews import validar_dados
+
+class ValidacaoTest(unittest.TestCase):
+    def test_validar(self):
+        contar_validas = 0
+        with open('noticias.json') as json_file:
+            noticias_mock = json.load(json_file)
+        for e in noticias_mock:
+            if validar_dados(e["link"],e["titulo"]):
+                contar_validas+=1
+        self.assertEqual(contar_validas,6,"Há 6 links válidos")
+
+if __name__ == '__main__':
+    unittest.main()
